0e4443088400b276524a21484f3b1a62ec0efd42
adilcse <adil.cs.work@gmail.com>
2020-09-04 21:55:40 +0530

    api key seeder added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(CatagorySeeder::class);
-        // $this->call(ApiKeySeeder::class);
-        $this->call(SliderSeeder::class);
+        $this->call(ApiKeySeeder::class);
+        // $this->call(SliderSeeder::class);
     }
 }
